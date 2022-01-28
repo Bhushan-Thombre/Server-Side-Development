0428e708705b1da8dd49caf187cba820ada48105
Bhushan Thombre <bhushanthombre2001@gmail.com>
2022-01-28 14:37:00 +0530

    Assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,6 +16,13 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/dishes/:dishId', dishRouter);
+
+app.use('/promotions', promoRouter);
+app.use('/promotions/:promoId', promoRouter);
+
+app.use('/leaders', leaderRouter);
+app.use('/leaders/:leaderId', leaderRouter);
 
 
 
