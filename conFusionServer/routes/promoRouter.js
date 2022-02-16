d545b5bed5d7684dc11698be722daad5f034f5a8
Bhushan Thombre <bhushanthombre2001@gmail.com>
2022-02-16 11:43:26 +0530

    Passport JWT

--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+var authenticate = require('../authenticate');
 
 const Promos = require('../models/promotions');
 
@@ -19,12 +20,12 @@ promoRouter.route('/')
     .catch((err) => next(err));
 })
 
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
 
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Promos.create(req.body)
     .then((promo) => {
         console.log('Promo Created', promo);
@@ -35,7 +36,7 @@ promoRouter.route('/')
     .catch((err) => next(err));
 })
 
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Promos.remove({})
     .then((promo) => {
         res.statusCode = 200;
@@ -56,12 +57,12 @@ promoRouter.route('/:promoId')
     .catch((err) => next(err));
 })
 
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /promotions/' + req.params.promoId);
 })
 
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Promos.findByIdAndUpdate(req.params.promoId, {
         $set: req.body
     }, { new: true })
@@ -73,7 +74,7 @@ promoRouter.route('/:promoId')
     .catch((err) => next(err));
 })
 
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Promos.findByIdAndRemove(req.params.promoId)
     .then((promo) => {
         res.statusCode = 200;
