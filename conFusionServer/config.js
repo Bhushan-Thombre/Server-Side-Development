8763bca33ba5d90683bdb7f2483f3ad826f86830
Bhushan Thombre <bhushanthombre2001@gmail.com>
2022-03-15 20:37:30 +0530

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '384931409804624',
+        clientSecret: '943b72bdf1cb7cf761ad7041cf25d0a1'
+    }
 }
