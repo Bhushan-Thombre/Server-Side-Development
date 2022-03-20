3e13c80df8cfa9c4c3cf1ef2b142cd96216a0d95
Bhushan Thombre <bhushanthombre2001@gmail.com>
2022-03-20 11:51:31 +0530

    Integrating Client and Server

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -16,6 +16,7 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var uploadRouter = require('./routes/uploadRouter');
 var favouriteRouter = require('./routes/favouriteRouter');
+var commentRouter = require('./routes/commentRouter');
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -89,6 +90,7 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
 app.use('/favorites', favouriteRouter);
+app.use('/comments', commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -9,6 +9,7 @@ const cors = require('./cors');
 router.use(bodyParser.json());
 
 /* GET users listing. */
+router.options('*', cors.corsWithOptions, (req, res) => { res.statusCode(200); });
 router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
   res.send('respond with a resource');
 });
@@ -47,13 +48,39 @@ router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   });
 });
 
-router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res, next) => {
+router.post('/login', cors.corsWithOptions, (req, res, next) => {
   
-  var token = authenticate.getToken({_id: req.user._id});
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, status: 'Login Unsuccessful!', err: info});
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: false, status: 'Login Unsuccessful!', err: 'Could not login user'});
+      }
+
+      var token = authenticate.getToken({_id: req.user._id});
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: true, status: 'Login Unsuccessful!', token: token });
+    });
+
+    
+
+
+  }) (req, res, next);
+
   
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token,  status: 'You are successfully logged in!'});
+  
 
 });
 
@@ -78,5 +105,24 @@ router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res
     res.json({success: true, token: token, status: 'You are successfully logged in!'});
   }
 });
+
+
+router.get('/checkJWTToken', cors.corsWithOptions, (req, res) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+     if (err) {
+       return next(err);
+     }
+
+     if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT invalid', success: false, err: info});
+     }else {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT valid', success: true, user: user});
+     }
+  }) (req, res);
+})
 
 module.exports = router;
